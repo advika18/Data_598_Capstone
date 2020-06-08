d7ded129d8f2725df073f330c357f87ddf986025
Advika Battini <advika.af502@gmail>
2020-06-09 02:37:36 +0530

    updated var model, impute na

--- code/var_model.R
+++ code/var_model.R
@@ -34,19 +34,31 @@ stocks <- stocks %>%
 stocks_train <- stocks %>% filter(Date<'2019-11-01')
 stocks_test <- stocks %>% filter(Date>='2019-11-01')
 
+##handling na values
+stocks_train <- stocks_train %>% 
+  fill(-'Date') %>% #default direction down
+  fill(-'Date',.direction = "updown")
+
 ## check johanson test of cointegration. Not cointegrated can use var
 summary(ca.jo(select(stocks,2,3,4,5), type="trace", K=2, ecdet="none", spec="longrun"))
 
+
 ##requires differencing to make it stationary
 num_diff <- 1 #single differencing for most of the series
-stocks_train_diff <- stocks_train %>% select(Date, '6', '8') %>% drop_na() ##only checking for two stocks right now, how to tackle NA
+stocks_train_diff <- stocks_train #%>% 
+  #select(-c('16','17','18','19','20','21')) %>% 
+  #drop_na() ##only checking for some stocks right now, how to tackle NA
+#stocks_train_diff <- stocks_train %>% drop_na() 
+
 stocks_train_diff <- diff(as.matrix(select(stocks_train_diff,-Date)), differences = num_diff)
-      
+
 
 require(vars) ##installing it later because it clashes with dplyr select
-var_model <- VAR(stocks_train_diff, lag.max = 12)
+var_model <- VAR(stocks_train_diff, lag.max = 2)
 
 future <- predict(var_model, n.ahead=14)
+
+### testing for two stocks
 just_one <- data.frame(future$fcst$X8)
 test_one <- just_one$fcst
 #last value of train set
@@ -55,5 +67,15 @@ crps_sample(stocks_test$`8`, matrix(pred_one,nrow = length(pred_one),ncol=1))
 
 base_one <- rep(133.54, 14)
 crps_sample(stocks_test$`8`, matrix(base_one,nrow = length(base_one),ncol=1))
+
+future <- predict(var_model, n.ahead=13)
+just_one <- data.frame(future$fcst$X6)
+test_one <- just_one$fcst
+#last value of train set
+pred_one <- 53.24 + cumsum(test_one)
+crps_sample(stocks_test$`6`[1:13], matrix(pred_one,nrow = length(pred_one),ncol=1), )
+
+base_one <- rep(53.24, 13)
+crps_sample(stocks_test$`6`[1:13], matrix(base_one,nrow = length(base_one),ncol=1))
 
   
